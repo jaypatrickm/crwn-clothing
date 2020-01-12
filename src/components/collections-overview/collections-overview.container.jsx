5d6033ad75b447f45853db5d92fff6917444d6ac
JAYPATRICK MANALANSAN <jaypatrickm@gmail.com>
2020-01-11 23:26:54 -0800

    update collections to use container pattern

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../with-spinner/with-sipinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: selectIsCollectionFetching
+});
+
+const CollectionsOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionsOverview);
+// const CollectionsOverviewContainer = connect(mapStateToProps)(WithSpinner(CollectionOverview))
+
+export default CollectionsOverviewContainer;
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop.selectors';
-
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import {
